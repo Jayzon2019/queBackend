7dbaf9b242dbc7da50436c54f675de688ccecc6f
jason.sanjose <jason.sanjose>
2025-01-17 23:12:58 +0800

    20250117 - 1

--- controllers/videoController.go
+++ controllers/videoController.go
@@ -3,40 +3,13 @@ package controllers
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-// VideoHandler serves the video file to the client
-func VideoHandler(c *fiber.Ctx) error {
-	// Get the video filename from URL
-
-	videoFile := c.Params("filename")
-
-	// Ensure the file exists
-	if _, err := os.Stat(videoFile); err != nil {
-		return c.Status(fiber.StatusNotFound).SendString("Video not found")
-	}
-
-	// Open the video file
-	file, err := os.Open(videoFile)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString("Could not open video file")
-	}
-	defer file.Close()
-
-	// Set headers for video streaming
-	c.Set("Content-Type", "video/mp4")
-	c.Set("Content-Disposition", fmt.Sprintf("inline; filename=%s", filepath.Base(videoFile)))
-
-	// Serve the video file content to the client
-	return c.SendStream(file)
-}
-
 func VideoHandler1(c *fiber.Ctx) error {
 	// Path to the video file
-	videoPath := "./sample.mp4" // Adjust this path to your video file location
+	videoPath := "./video/sample.mp4" // Adjust this path to your video file location
 
 	// Open the video file
 	file, err := os.Open(videoPath)
@@ -62,7 +35,7 @@ func VideoHandler1(c *fiber.Ctx) error {
 
 func VideoHandler2(c *fiber.Ctx) error {
 	// Path to the video file
-	videoPath := "./video2.mp4" // Adjust this path to your video file location
+	videoPath := "./video/video2.mp4" // Adjust this path to your video file location
 
 	// Open the video file
 	file, err := os.Open(videoPath)
